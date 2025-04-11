cc5297a2b8dbd6db061cae65b79231e0e1c0c4da
Hender Orlando <hender.puello@gmail.com>
2025-04-11 10:28:22 -0500

    Remove ExpressAdapter integration and related initialization from bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { ExpressAdapter } from "@nestjs/platform-express";
+/*import { ExpressAdapter } from "@nestjs/platform-express";*/
 import express from "express";
 import * as bodyParser from "body-parser";
 
@@ -11,7 +11,7 @@ expressApp.use(bodyParser.json());
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(expressApp),
+    /*new ExpressAdapter(expressApp),*/
   );
   app.enableCors();
   app.useGlobalPipes(
@@ -21,7 +21,7 @@ async function bootstrap() {
       transform: true, // importante para que convierta al tipo DTO
     }),
   );
-  await app.init();
+  /*await app.init();*/
   await app.listen(process.env.PORT ?? 3000);
 }
 
