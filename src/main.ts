686e0105660aad57f5fb86da0ff46cd8d669311a
Hender Orlando <hender.puello@gmail.com>
2025-04-11 10:43:11 -0500

    Refactor bootstrap function to support caching and update handler for Express integration

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import express from "express";
+import { Request, Response } from "express";
 import * as bodyParser from "body-parser";
 
 const expressApp = express();
 expressApp.use(bodyParser.json());
+let cachedApp: express.Express | null = null;
 
 async function bootstrap() {
   const app = await NestFactory.create(
@@ -23,8 +25,12 @@ async function bootstrap() {
   );
   await app.init();
   /*await app.listen(process.env.PORT ?? 3000);*/
+  return expressApp;
 }
 
-bootstrap();
-
-export const handler = expressApp;
+export default async function handler(req: Request, res: Response) {
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
+  }
+  return cachedApp(req, res);
+}
